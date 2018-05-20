240d05b10c02938254c524fbc5843e34f3c0538a
swphes <swpheus1@gmail.com>
2018-05-20 14:12:37 +0900

    second

--- src/blockchain.js
+++ src/blockchain.js
@@ -28,7 +28,7 @@ const getLastBlock=()=>blockchain[blockchain.length-1];
 const getTimestamp=()=> new Date().getTime() /1000;
 
 const createHash=(index,previousHash,timetamp,data)=>
-    CryptoJS.SHA256(index+previousHash+timetamp+data).toString();
+    CryptoJS.SHA256(index+previousHash+timetamp+JSON.stringify(data)).toString();
 
 const createNewBlock =data =>{
     const previousHash =getLastBlock();
@@ -69,6 +69,34 @@ const isNewBlockValid =(candidateBlock, latestBlock)=>{
         return false;
     }
     return true;
-}
+};
+
+const isNewStructureValid = block =>{
+    return(
+        typeof block.index ==="number"&&
+        typeof block.hash ==="string"&&
+        typeof block.previousHash ==="string" &&
+        typeof block.timetamp ==="number" &&
+        typeof block.data === "string"
+    );
+};
+
+const isChainValid =(candidateChain) =>{
+    const isGenesisValid = block =>{
+     return JSON.stringify(block) === JSON.stringify(genesisBlock);
+    };
+    if(!isGenesisValid(candidateChain[0])){
+        console.log("The candidateChains's genesisBlock is not the same as our genesis"
+        );
+    return false;    
+    }
+    for (let i=1; i < candidateChain.length; i++){
+        if(!isNewBlockValid(candidateChain[i],candidateChain[i-1])){
+            return false;
+        }
+    }
+    return true;
+};
+
 
 
